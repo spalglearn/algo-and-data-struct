f1227f05a15b0c8bb108dd37a34e56ceabeb0acc
Soundra Pandian <soundrapandian@outlook.com>
2019-02-27 11:56:46 +0530

    capitalize2

--- src/main/java/gr8/d/soundar/algods/Alg08Capitalize.java
+++ src/main/java/gr8/d/soundar/algods/Alg08Capitalize.java
@@ -2,23 +2,23 @@ package gr8.d.soundar.algods;
 
 public class Alg08Capitalize {
     public String capitalize(String s) {
-        boolean capitalizeNext = true;
         char[] array = s.toCharArray();
 
         for (int i = 0; i < array.length; i++) {
-            char c = array[i];
-
-            if (c == ' ') {
-                capitalizeNext = true;
-                continue;
-            }
-
-            if (capitalizeNext) {
+            if (i == 0 || array[i - 1] == ' ')
                 array[i] = Character.toUpperCase(array[i]);
-                capitalizeNext = false;
-            }
-            
         }
         return new String(array);
+    }
+
+    public String capitalize2(String s) {
+        String[] words = s.split(" ");
+        StringBuilder builder = new StringBuilder();
+        for (String word : words) {
+            builder.append(Character.toUpperCase(word.charAt(0)))
+                .append(word.substring(1, word.length()))
+                .append(" ");
+        }
+        return builder.toString().strip();
     }
 }
--- src/test/java/gr8/d/soundar/algods/Alg08CapitalizeTest.java
+++ src/test/java/gr8/d/soundar/algods/Alg08CapitalizeTest.java
@@ -9,4 +9,10 @@ public class Alg08CapitalizeTest {
         Assert.assertEquals("Am I There Yet", new Alg08Capitalize().capitalize("am i there yet"));
         Assert.assertEquals("Am I There, Yet", new Alg08Capitalize().capitalize("am i there, yet"));
     }
+
+    @Test
+    public void testCapitalize2() {
+        Assert.assertEquals("Am I There Yet", new Alg08Capitalize().capitalize2("am i there yet"));
+        Assert.assertEquals("Am I There, Yet", new Alg08Capitalize().capitalize2("am i there, yet"));
+    }
 }
