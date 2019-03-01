e4d7b200a2f9b55b835a60efbef28a804fa0c07d
Soundra Pandian <soundrapandian@outlook.com>
2019-03-01 13:47:24 +0530

    queue

---
+++ src/main/java/gr8/d/soundar/algods/Alg14Queue.java
@@ -0,0 +1,94 @@
+package gr8.d.soundar.algods;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Alg14Queue {
+
+    public static <T> Queue<T> weave (Queue<T> q1, Queue<T> q2) {
+        Queue<T> ret = new QueueUsingArray<>();
+        while (q1.size() > 0 || q2.size() > 0) {
+            if (q1.peek() != null)
+                ret.add(q1.remove());
+            if (q2.peek() != null)
+                ret.add(q2.remove());
+        }
+        return ret;
+    }
+
+    public static interface Queue<T extends Object> {
+        public void add(T t);
+        public T remove();
+        public T peek();
+        public int size();
+    }
+
+    public static class QueueUsingArray<T> implements Queue<T> {
+        private Object[] q = {};
+
+        @Override
+        public void add(T t) {
+            q = Arrays.copyOf(q, q.length + 1);
+            q[q.length - 1] = t;
+        }
+
+        @Override
+        public T remove() {
+            try {
+                T item = (T) q[0];
+                q = Arrays.copyOfRange(q, 1, q.length);
+                return item;
+            } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+                return null;
+            }
+        }
+
+        @Override
+        public int size() {
+            return q.length;
+        }
+
+        @Override
+        public T peek() {
+            try {
+                return (T) q[0];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return null;
+            }
+        }
+    }
+
+    public static class QueueUsingList<T> implements Queue<T> {
+
+        List<T> q = new ArrayList<>();
+
+        @Override
+        public void add(T t) {
+            q.add(t);
+        }
+
+        @Override
+        public T remove() {
+            try {
+                return q.remove(0);
+            } catch (IndexOutOfBoundsException e) {
+                return null;
+            }
+        }
+
+        @Override
+        public int size() {
+            return q.size();
+        }
+
+        @Override
+        public T peek() {
+            try {
+                return q.get(0);
+            } catch (IndexOutOfBoundsException e) {
+                return null;
+            }
+        }
+    }
+}
---
+++ src/test/java/gr8/d/soundar/algods/Alg14QueueTest.java
@@ -0,0 +1,49 @@
+package gr8.d.soundar.algods;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class Alg14QueueTest {
+    @Test
+    public void testQueueUsingArray() {
+        testQueue(new Alg14Queue.QueueUsingArray<>());
+    }
+
+    @Test
+    public void testQueueUsingList() {
+        testQueue(new Alg14Queue.QueueUsingList<>());
+    }
+
+    @Test
+    public void testWeave() {
+        Alg14Queue.Queue<Integer> q1 = new Alg14Queue.QueueUsingArray<>();
+        q1.add(1);
+        q1.add(2);
+        Alg14Queue.Queue<Integer> q2 = new Alg14Queue.QueueUsingArray<>();
+        q2.add(3);
+        q2.add(4);
+        q2.add(5);
+        Alg14Queue.Queue<Integer> weavedQueue = Alg14Queue.weave(q1, q2);
+        Assert.assertEquals(5, weavedQueue.size());
+        Assert.assertEquals(Integer.valueOf(1), weavedQueue.remove());
+        Assert.assertEquals(Integer.valueOf(3), weavedQueue.remove());
+        Assert.assertEquals(Integer.valueOf(2), weavedQueue.remove());
+        Assert.assertEquals(Integer.valueOf(4), weavedQueue.remove());
+        Assert.assertEquals(Integer.valueOf(5), weavedQueue.remove());
+    }
+
+    private void testQueue(Alg14Queue.Queue<Integer> queue) {
+        Assert.assertEquals(0, queue.size());
+        queue.add(1);
+        Assert.assertEquals(1, queue.size());
+        Assert.assertEquals(Integer.valueOf(1), queue.peek());
+        queue.add(2);
+        Assert.assertEquals(Integer.valueOf(1), queue.peek());
+        Assert.assertEquals(2, queue.size());
+        Assert.assertEquals(Integer.valueOf(1), queue.remove());
+        Assert.assertEquals(1, queue.size());
+        Assert.assertEquals(Integer.valueOf(2), queue.peek());
+        Assert.assertEquals(Integer.valueOf(2), queue.remove());
+        Assert.assertEquals(0, queue.size());
+    }
+}
