3c5528efc65cc0b3d9874df6ff839e6850b8723b
Soundra Pandian <soundrapandian@outlook.com>
2019-03-05 14:15:33 +0530

    linked list

---
+++ src/main/java/gr8/d/soundar/algods/Alg16LinkedList.java
@@ -0,0 +1,178 @@
+package gr8.d.soundar.algods;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Alg16LinkedList {
+
+    public static class LinkedList<T> implements Iterable<T> {
+        private static final int LAST_INDEX = -1;
+        private Node<T> headNode;
+
+        public void insertFirst(Node<T> node) {
+            insertAt(0, node);
+        }
+
+        public void insertLast(Node<T> node) {
+            node.setNext(null);
+            insertAt(LAST_INDEX, node);
+        }
+
+        public void insertAt(int index, Node<T> node) {
+            if (node == null) {
+                return;
+            }
+
+            node.setNext(null);
+
+            if (headNode == null) {
+                // Empty linked list
+                headNode = node;
+            } else if (index == 0) {
+                // Insert at head
+                node.setNext(headNode);
+                headNode = node;
+                return;
+            } else {
+                List<Node<T>> indexedNodes = indexiefy();
+                if (index == LAST_INDEX) {
+                    // Insert at last
+                    try {
+                        indexedNodes.get(indexedNodes.size() - 1).setNext(node);
+                    } catch (IndexOutOfBoundsException e) {
+                        // If this is the case, current node is head
+                        headNode = node;
+                    }
+                } else {
+                    // Insert at any other position
+                    Node<T> nextNode = indexedNodes.get(index);
+                    node.setNext(nextNode);
+                    try {
+                        Node<T> prevNode = indexedNodes.get(index - 1);
+                        prevNode.setNext(node);
+                    } catch (IndexOutOfBoundsException e) {
+                        // If this is the case, current node is head
+                        headNode = node;
+                    }
+                }
+            }
+        }
+
+        public Node<T> getFirst() {
+            return getAt(0);
+        }
+
+        public Node<T> getLast() {
+            return getAt(LAST_INDEX);
+        }
+
+        public Node<T> getAt(int index) {
+            List<Node<T>> indexedNodes = indexiefy();
+            if (index == LAST_INDEX)
+                index = indexedNodes.size() - 1;
+            try {
+                return indexedNodes.get(index);
+            } catch (IndexOutOfBoundsException e) {
+                return null;
+            }
+        }
+
+        public void removeFirst() {
+            removeAt(0);
+        }
+
+        public void removeLast() {
+            removeAt(LAST_INDEX);
+        }
+
+        public void removeAt(int index) {
+            List<Node<T>> indexedNodes = indexiefy();
+            if (index == LAST_INDEX)
+                index = indexedNodes.size() - 1;
+            Node<T> currNode = indexedNodes.get(index);
+            Node<T> nextNode = currNode.getNext();
+            try {
+                Node<T> prevNode = indexedNodes.get(index - 1);
+                prevNode.setNext(nextNode);
+            } catch (IndexOutOfBoundsException e) {
+                // If no previous node, next node is head
+                headNode = nextNode;
+            }
+        }
+
+        public int size() {
+            int size = 0;
+            Node<T> node = headNode;
+            while (node != null) {
+                size++;
+                node = node.getNext();
+            }
+            return size;
+        }
+
+        public void clear() {
+            headNode = null;
+        }
+
+        private List<Node<T>> indexiefy() {
+            List<Node<T>> index = new ArrayList<>();
+
+            Node<T> node = headNode;
+
+            while (node != null) {
+                index.add(node);
+                node = node.getNext();
+            }
+
+            return index;
+        }
+
+        @Override
+        public Iterator<T> iterator() {
+            return new LinkedListIterator();
+        }
+
+        class LinkedListIterator implements Iterator<T> {
+
+            private Node<T> currNode = headNode;
+
+            @Override
+            public boolean hasNext() {
+                return (currNode == null) ? false : currNode.getNext() != null;
+            }
+
+            @Override
+            public T next() {
+                currNode = (currNode == null) ? null : currNode.getNext();
+                return currNode.getData();
+            }
+        }
+    }
+
+    public static class Node<T> {
+        private T data;
+        private Node<T> next;
+        
+        public Node(T data) {
+            this(data, null);
+        }
+
+        public Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public T getData() {
+            return data;
+        }
+    }
+}
---
+++ src/test/java/gr8/d/soundar/algods/Alg16LinkedListTest.java
@@ -0,0 +1,158 @@
+package gr8.d.soundar.algods;
+
+import java.util.Iterator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Alg16LinkedListTest {
+    @Test 
+    public void testInsertFirst() {
+        Alg16LinkedList.LinkedList<Integer> ll = new Alg16LinkedList.LinkedList<>();
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(3)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(2)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(1)));
+        Alg16LinkedList.Node<Integer> node = ll.getFirst();
+        Assert.assertEquals(Integer.valueOf(1), node.getData());
+        node = node.getNext();
+        Assert.assertEquals(Integer.valueOf(2), node.getData());
+        node = node.getNext();
+        Assert.assertEquals(Integer.valueOf(3), node.getData());
+        Assert.assertNull(node.getNext());
+    }
+
+    @Test 
+    public void testInsertLast() {
+        Alg16LinkedList.LinkedList<Integer> ll = new Alg16LinkedList.LinkedList<>();
+        ll.insertLast(new Alg16LinkedList.Node<Integer>(Integer.valueOf(1)));
+        ll.insertLast(new Alg16LinkedList.Node<Integer>(Integer.valueOf(2)));
+        ll.insertLast(new Alg16LinkedList.Node<Integer>(Integer.valueOf(3)));
+        Alg16LinkedList.Node<Integer> node = ll.getFirst();
+        Assert.assertEquals(Integer.valueOf(1), node.getData());
+        node = node.getNext();
+        Assert.assertEquals(Integer.valueOf(2), node.getData());
+        node = node.getNext();
+        Assert.assertEquals(Integer.valueOf(3), node.getData());
+        Assert.assertNull(node.getNext());
+    }
+
+    @Test 
+    public void testInsertAt() {
+        Alg16LinkedList.LinkedList<Integer> ll = new Alg16LinkedList.LinkedList<>();
+        ll.insertLast(new Alg16LinkedList.Node<Integer>(Integer.valueOf(1)));
+        ll.insertLast(new Alg16LinkedList.Node<Integer>(Integer.valueOf(2)));
+        ll.insertLast(new Alg16LinkedList.Node<Integer>(Integer.valueOf(3)));
+        ll.insertAt(1, new Alg16LinkedList.Node<>(Integer.valueOf(5)));
+        Alg16LinkedList.Node<Integer> node = ll.getFirst();
+        Assert.assertEquals(Integer.valueOf(1), node.getData());
+        node = node.getNext();
+        Assert.assertEquals(Integer.valueOf(5), node.getData());
+        node = node.getNext();
+        Assert.assertEquals(Integer.valueOf(2), node.getData());
+        node = node.getNext();
+        Assert.assertEquals(Integer.valueOf(3), node.getData());
+        Assert.assertNull(node.getNext());
+    }
+
+    @Test
+    public void testGetFirst() {
+        Alg16LinkedList.LinkedList<Integer> ll = new Alg16LinkedList.LinkedList<>();
+        Assert.assertNull(ll.getLast());
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(3)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(2)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(1)));
+        Assert.assertEquals(Integer.valueOf(1), ll.getFirst().getData());
+    }
+
+    @Test
+    public void testGetLast() {
+        Alg16LinkedList.LinkedList<Integer> ll = new Alg16LinkedList.LinkedList<>();
+        Assert.assertNull(ll.getLast());
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(3)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(2)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(1)));
+        Assert.assertEquals(Integer.valueOf(3), ll.getLast().getData());
+    }
+
+    @Test 
+    public void testGetAt() {
+        Alg16LinkedList.LinkedList<Integer> ll = new Alg16LinkedList.LinkedList<>();
+        ll.insertLast(new Alg16LinkedList.Node<Integer>(Integer.valueOf(1)));
+        ll.insertLast(new Alg16LinkedList.Node<Integer>(Integer.valueOf(2)));
+        ll.insertLast(new Alg16LinkedList.Node<Integer>(Integer.valueOf(3)));
+        Assert.assertEquals(Integer.valueOf(2), ll.getAt(1).getData());
+    }
+
+    @Test
+    public void testRemoveFirst() {
+        Alg16LinkedList.LinkedList<Integer> ll = new Alg16LinkedList.LinkedList<>();
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(3)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(2)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(1)));
+        ll.removeFirst();
+        Assert.assertEquals(Integer.valueOf(2), ll.getFirst().getData());
+    }
+
+    @Test
+    public void testRemoveLast() {
+        Alg16LinkedList.LinkedList<Integer> ll = new Alg16LinkedList.LinkedList<>();
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(3)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(2)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(1)));
+        ll.removeLast();
+        Assert.assertEquals(Integer.valueOf(2), ll.getLast().getData());
+    }
+
+    @Test 
+    public void testRemoveAt() {
+        Alg16LinkedList.LinkedList<Integer> ll = new Alg16LinkedList.LinkedList<>();
+        ll.insertLast(new Alg16LinkedList.Node<Integer>(Integer.valueOf(1)));
+        ll.insertLast(new Alg16LinkedList.Node<Integer>(Integer.valueOf(2)));
+        ll.insertLast(new Alg16LinkedList.Node<Integer>(Integer.valueOf(3)));
+        ll.removeAt(1);
+        Alg16LinkedList.Node<Integer> node = ll.getFirst();
+        Assert.assertEquals(Integer.valueOf(1), node.getData());
+        node = node.getNext();
+        Assert.assertEquals(Integer.valueOf(3), node.getData());
+        Assert.assertNull(node.getNext());
+    }
+
+    @Test
+    public void testSize() {
+        Alg16LinkedList.LinkedList<Integer> ll = new Alg16LinkedList.LinkedList<>();
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(3)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(2)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(1)));
+        Assert.assertEquals(3, ll.size());
+    }
+
+    @Test
+    public void testClear() {
+        Alg16LinkedList.LinkedList<Integer> ll = new Alg16LinkedList.LinkedList<>();
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(3)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(2)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(1)));
+        ll.clear();
+        Assert.assertEquals(0, ll.size());
+    }
+
+    @Test
+    public void testIterator() {
+        Alg16LinkedList.LinkedList<Integer> ll = new Alg16LinkedList.LinkedList<>();
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(3)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(2)));
+        ll.insertFirst(new Alg16LinkedList.Node<Integer>(Integer.valueOf(1)));
+
+        for (Integer i : ll) {
+            Assert.assertNotNull(i);
+            Assert.assertTrue(i.equals(1) || i.equals(2) || i.equals(3));
+        }
+
+        Iterator<Integer> lli = ll.iterator();
+        while (lli.hasNext()) {
+            Integer i = lli.next();
+            Assert.assertNotNull(i);
+            Assert.assertTrue(i.equals(1) || i.equals(2) || i.equals(3));
+        }
+    }
+}
